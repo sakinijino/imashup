8319d4bee7d32d543eb2373efa91df176089a675
saki <sakinijino@gmail.com>
2011-01-11 15:25:40 +0800

    Del some useless files. Add README and Screen Casts

--- components/igrid/all.js
+++
@@ -1,5 +0,0 @@
-dojo.provide("imashup.components.igrid.all");
-
-dojo.require("imashup.components.igrid.PlatformLister")
-dojo.require("imashup.components.igrid.ApplicationLister")
-dojo.require("imashup.components.igrid.TomcatManager")
