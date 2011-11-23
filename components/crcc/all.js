81d5568ac91bd4dbf1841db41087b8875f53adb5
saki <sakinijino@gmail.com>
2011-11-23 11:00:09 +0800

    disabled local storage for mobile mode.

--- components/crcc/all.js
+++ components/crcc/all.js
@@ -6,6 +6,9 @@ dojo.require("imashup.components.crcc.Stocks");
 dojo.require("imashup.components.crcc.PieChart");
 dojo.require("imashup.components.crcc.BarChart");
 dojo.require("imashup.components.crcc.GoogleMap");
-dojo.require("imashup.components.crcc.Todos");
-dojo.require("imashup.components.crcc.Contacts");
-dojo.require("imashup.components.crcc.Inbox");
+
+if (!imashup.mobileMode) {
+  dojo.require("imashup.components.crcc.Todos");
+  dojo.require("imashup.components.crcc.Contacts");
+  dojo.require("imashup.components.crcc.Inbox");
+}
